0a8665c7484b2a7ce0926895eedc5fb3196573aa
kujgb <orazamina7@gmail.com>
2024-01-31 10:39:16 +0600

    lab2

---
+++ lab2/Operators/example2.py
@@ -0,0 +1,14 @@
+#1 
+print(10 + 5)
+
+#2 Parentheses has the highest precedence, meaning that expressions inside parentheses must be evaluated first:
+
+print((6 + 3) - (6 + 3))
+
+#3 Multiplication * has higher precedence than addition +, and therefor multiplications are evaluated before additions:
+
+print(100 + 5 * 3)
+
+#4 Addition + and subtraction - has the same precedence, and therefor we evaluate the expression from left to right:
+
+print(5 + 4 - 7 + 3)
