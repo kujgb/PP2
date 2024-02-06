79c2fff0979b91df62c7d5319cd2723dc974e675
kujgb <orazamina7@gmail.com>
2024-02-06 17:10:41 +0600

    lab3

---
+++ lab3/Classes/example1.py
@@ -0,0 +1,91 @@
+#Create a class named MyClass, with a property named x:
+
+class MyClass:
+  x = 5
+
+#Example
+#Create an object named p1, and print the value of x:
+
+p1 = MyClass()
+print(p1.x)
+
+#Example
+#Create a class named Person, use the __init__() function to assign values for name and age:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+#Example
+#The string representation of an object WITHOUT the __str__() function:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+
+#Example
+#The string representation of an object WITH the __str__() function:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+#Example
+#Insert a function that prints a greeting, and execute it on the p1 object:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#Example
+#Use the words mysillyobject and abc instead of self:
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#Set the age of p1 to 40:
+p1.age = 40
+
+#Example
+del p1.age
+
+#Delete the p1 object:
+del p1
+
+#Example
+class Person:
+  pass
